5201ce5785d83843b4edd6ce4c1825a193e6ba21
Ricardo Rodriguez <ricardo.rodriguez252002@gmail.com>
2025-11-22 18:54:22 -0500

    callback route fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,4 @@
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -8,8 +7,7 @@ export async function GET(request: Request) {
   const next = searchParams.get("next") ?? "/dashboard";
 
   if (code) {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
+    const supabase = await createClient();
     
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     
